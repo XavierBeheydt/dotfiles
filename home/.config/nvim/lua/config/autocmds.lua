3415882cf2343d7b5021aa8f5b988b4b6902a873
Xavier Beheydt <xavier.beheydt@gmail.com>
2026-09-17 11:15:26 +0200

    docs: Simplify the explication

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,6 @@
 -- Copyright (c) 2026 Xavier Beheydt <xavier.beheydt@gmail.com>
 
--- Shada (which restores g:RELATIVENUMBER, see config.options) is loaded
--- after this config is sourced, so re-apply the persisted value once
--- VimEnter fires and shada is guaranteed to be loaded.
+-- Settings restoration at Vim entering
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         vim.opt.relativenumber = vim.g.RELATIVENUMBER
