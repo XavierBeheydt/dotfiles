99946edd9e99da6c8e03e4bf74c323e2c811f839
Xavier Beheydt <xavier.beheydt@gmail.com>
2026-09-18 15:08:03 +0200

    docs: list alternative Lua formatters in formatters/lua.lua

--- home/.config/nvim/formatters/lua.lua
+++ home/.config/nvim/formatters/lua.lua
@@ -1,4 +1,11 @@
 -- Copyright (c) 2026 Xavier Beheydt <xavier.beheydt@gmail.com>
 
+-- Other possible formatters for Lua:
+--   "lsp"                                -> use lua_language_server's own formatter instead
+--   { cmd = { "lua-format", ... } }      -> Koihik/LuaFormatter (C++, .lua-format config file)
+--   { cmd = { "lua-fmt", ... } }         -> trixnz/lua-fmt (Node.js, wraps LuaFormatter)
+-- (neither lua-format nor lua-fmt is installed here, and their stdin/stdout
+-- flags aren't verified like stylua's -- check `--help` before switching.)
+
 -- stylua (external) instead of the LSP formatter.
 return { cmd = { "stylua", "-" } }
